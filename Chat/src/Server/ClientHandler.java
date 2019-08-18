434a8c618cb1299356609f642760d26ad32909fe
Rarik <reuderagent@gmail.com>
2019-08-18 16:18:21 +0300

    Домашняя работа №7

---
+++ Chat/src/Server/ClientHandler.java
@@ -0,0 +1,88 @@
+package Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    private Server server;
+    private Socket socket;
+    DataInputStream in;
+    DataOutputStream out;
+    String nick;
+    private String name;
+
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.name = name;
+            this.server = server;
+            this.socket = socket;
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                try {
+                    // цикл авторизации
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/auth ")) {
+                            String[] token = str.split(" ");
+                            String newNick = AuthService.getNickByLoginAndPass(token[1], token[2]);
+                            if (newNick != null) {
+                                sendMSG("/authok");
+                                nick = newNick;
+                                server.subscribe(this);
+                                System.out.println("Клиент " + nick + " авторизовался");
+                                break;
+                            } else {
+                                sendMSG("Неверный логин / пароль");
+                            }
+                        }
+                    }
+                    //цикл работы
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.equals("/end")) {
+                            break;
+                        }
+                        if (str.equals("/w ")) {
+                            String[] token = str.split(" ");
+                            String name = token[1];
+                            String msg = token[2];
+                            server.tellMsg(nick, msg);
+                        } else {
+                            server.broadcastMsg(nick + " : " + str);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    server.unsubscribe(this);
+                    System.out.println("Клиент " + nick + " отключился");
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMSG(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String getName() {
+        return this.name;
+    }
+
+}
+
---
+++ Chat/src/Server/Server.java
@@ -0,0 +1,62 @@
+package Server;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.Vector;
+
+public class Server {
+    private Vector<ClientHandler> clients;
+
+    public Server() throws SQLException {
+        AuthService.connect();
+
+        clients = new Vector<>();
+        ServerSocket server = null;
+        Socket socket = null;
+        try {
+            server = new ServerSocket(9189);
+            System.out.println("Сервер запущен");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+                new ClientHandler(this, socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+        }
+    }
+    public void tellMsg(String name, String str){
+        for (ClientHandler c:clients ) {
+            if (c.getName().equals(name)) {
+                c.sendMSG(str);
+            }
+        }
+    }
+
+    public void broadcastMsg(String msg){
+        for (ClientHandler c:clients ) {
+            c.sendMSG(msg);
+        }
+    }
+
+    public void subscribe(ClientHandler clientHandler){
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler){
+        clients.remove(clientHandler);
+    }
+
+}
+
