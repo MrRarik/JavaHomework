73a5ea7dca092e9b2bc14cb47c16063fc8c4c7f4
Rarik <reuderagent@gmail.com>
2019-08-20 13:22:38 +0300

    Домашняя работа №8

---
+++ Chat/src/client/Main.java
@@ -0,0 +1,29 @@
+/**
+ * lesson 7 level 2
+ * @author Maluy Yaroslav
+ * @version data 19.08.19
+ */
+
+package client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Super chat");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
